8243d5ea243571eac914d3da62bffafab32eff82
Jonathan Cohlmeyer <jcohlmeyer@jrc9.ca>
2018-03-17 21:34:21 +0000

    Change namespace to be consistant with github and abstract settings into Statamic Settings

--- src/AnalyticsServiceProvider.php
+++ src/AnalyticsServiceProvider.php
@@ -7,6 +7,8 @@ use JRC9DS\Analytics\Exceptions\InvalidConfiguration;
 
 class AnalyticsServiceProvider extends ServiceProvider
 {
+    private $google_analytics;
+
     /**
      * Bootstrap the application events.
      */
@@ -22,29 +24,9 @@ class AnalyticsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $analyticsConfig = [
-            /*
-             * The view id of which you want to display data.
-             */
-            'view_id' => env('GOOGLE_ANALYTICS_VIEW_ID'),
+        $this->google_analytics = new \Statamic\Addons\GoogleAnalytics\GoogleAnalyticsAPI;
 
-            /*
-             * Path to the json file with service account credentials. Take a look at the README of this package
-             * to learn how to get this file.
-             */
-            'service_account_credentials_json' => storage_path('../../site/settings/addons/google-analytics-credentials.json'),
-
-            /*
-             * The amount of minutes the Google API responses will be cached.
-             * If you set this to zero, the responses won't be cached at all.
-             */
-            'cache_lifetime_in_minutes' => 60 * 24,
-
-            /*
-             * The directory where the underlying Google_Client will store it's cache files.
-             */
-            'cache_location' => storage_path('../local/cache/laravel-google-analytics/google-cache/'),
-        ];
+        $analyticsConfig = $this->google_analytics->analyticsConfig();
 
         $this->app->bind(AnalyticsClient::class, function () use ($analyticsConfig) {
             return AnalyticsClientFactory::createForConfig($analyticsConfig);
